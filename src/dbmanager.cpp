fe304ca3a20babcdf2953ba7925fc4e064af4311
Fynn <enriquefynn@gmail.com>
2014-10-22 08:00:21 -0300

    email as foreign key

--- src/dbmanager.cpp
+++ src/dbmanager.cpp
@@ -13,7 +13,7 @@ bool DBManager::openDB()
         qDebug() << "Creating tables" << endl;
         query.exec("CREATE TABLE climber (name VARCHAR(32), phone VARCHAR(16), address VARCHAR(40),\
                    email VARCHAR(50), expirationDate DATE, startDate DATE, status CHAR(1), PRIMARY KEY (email))");
-        query.exec("CREATE TABLE payment (id INTEGER PRIMARY KEY, email TEXT, paymentDate DATE, expirationDate DATE, value NUMERIC)");
+        query.exec("CREATE TABLE payment (id INTEGER PRIMARY KEY, email TEXT, paymentDate DATE, expirationDate DATE, value NUMERIC, FOREIGN KEY(email) REFERENCES climber(email))");
     }
     else
         status = db.open();
